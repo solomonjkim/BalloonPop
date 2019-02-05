f94ae43084652917e71b0eedd124f63ecc5dbb20
Solomon <solomon@somosoft.com>
2019-02-05 16:04:56 -0500

    initial commit

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    public float balloonSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float changeInHeight = Time.deltaTime * balloonSpeed;
+
+        Vector3 positionOffset = new Vector3(0, changeInHeight, 0);
+
+        transform.position += positionOffset;
+    }
+}
